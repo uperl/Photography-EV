97c97b02f98d802926e9744620335440343e002c
Graham Ollis <perl@wdlabs.com>
2014-09-18 11:31:16 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,40 @@
+# Photography::EV
+
+Calculate exposure value (EV)
+
+# SYNOPSIS
+
+    use Photography::EV;
+    my $ev = ev(5.6, 1/1000); # EV for f/5.6 at 1/1000s
+
+# DESCRIPTION
+
+This module provides functions for calculating photographic
+exposure values.  On some cameras the exposure can be locked
+into a specific Exposure Value (EV), such that changing the
+aperture or shutter speed will adjust the shutter speed or
+aperture to maintain the same exposure.
+
+# FUNCTIONS
+
+## ev
+
+    my $ev = ev($aperture, $time);
+
+Takes the aperture (f-stop) and shutter speed (in seconds).
+Returns the Exposure Value (EV).
+
+# SEE ALSO
+
+- http://en.wikipedia.org/wiki/Exposure\_value
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Photography/EV.pm
+++ lib/Photography/EV.pm
@@ -2,8 +2,66 @@ package Photography::EV;
 
 use strict;
 use warnings;
+use v5.20;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+use base qw( Exporter );
+
+our @EXPORT_OK = qw( ev );
+our @EXPORT = @EXPORT_OK;
 
 # ABSTRACT: Calculate exposure value (EV)
 # VERSION
 
+=head1 SYNOPSIS
+
+ use Photography::EV;
+ my $ev = ev(5.6, 1/1000); # EV for f/5.6 at 1/1000s
+
+=head1 DESCRIPTION
+
+This module provides functions for calculating photographic
+exposure values.  On some cameras the exposure can be locked
+into a specific Exposure Value (EV), such that changing the
+aperture or shutter speed will adjust the shutter speed or
+aperture to maintain the same exposure.
+
+=cut
+
+sub _round ($)
+{
+  $_[0] ? int($_[0] + $_[0]/abs($_[0]*2)) : 0;
+}
+
+sub _log2 ($)
+{
+  return log($_[0])/log(2);
+}
+
+=head1 FUNCTIONS
+
+=head2 ev
+
+ my $ev = ev($aperture, $time);
+
+Takes the aperture (f-stop) and shutter speed (in seconds).
+Returns the Exposure Value (EV).
+
+=cut
+
+sub ev ($aperture, $time)
+{
+  _round _log2 $aperture*$aperture/$time;
+}
+
 1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item http://en.wikipedia.org/wiki/Exposure_value
+
+=back
+
+=cut
